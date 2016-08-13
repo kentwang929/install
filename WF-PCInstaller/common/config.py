fb4cf77127a7ab2fd4aa5697f35efd7affb46cad
Chung Kang Wang <chung@wavefront.com>
2016-08-13 00:02:01 +0000

    refactor interface: write_plugin -> collect_data and output_config. telegraf plugins and refactor: nginx and apache

--- WF-PCInstaller/common/config.py
+++ WF-PCInstaller/common/config.py
@@ -1,6 +1,6 @@
 # install script will pass in the name of log file
 INSTALL_LOG = None
-DEBUG = True
+DEBUG = False
 OPERATING_SYSTEM = None
 AGENT = None
 DEBIAN = 'DEBIAN'
@@ -15,7 +15,7 @@ COLLECTD_CONF_DIR = COLLECTD_HOME + '/managed_config'
 COLLECTD_INSTALL_STATE_FILE_PATH = '{}/{}'.format(
     COLLECTD_CONF_DIR, INSTALL_STATE_FILE)
 COLLECTD_PLUGIN_DIR = 'plugin_dir/collectd'
-
+COLLECTD_PYTHON_PLUGIN_PATH = '/opt/collectd/plugins/python'
 # telegraf
 TELEGRAF_HOME = '/etc/telegraf'
 TELEGRAF_CONF_DIR = TELEGRAF_HOME + '/telegraf.d'
@@ -28,4 +28,4 @@ PLUGINS_FILE = 'support_plugins.json'
 PLUGINS_FILE_PATH= 'python_installer/config/' + PLUGINS_FILE
 PLUGIN_CONF_DIR = 'plugin_conf'
 PLUGIN_EXTENSION_DIR = 'plugin_extension'
-APP_DIR = '/tmp/app_configure'
+APP_DIR = '/tmp/WF-CDPInstaller'
